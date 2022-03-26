e5d6230eb52842b9038bd82711fc4878dd69b61f
Miyauchi <hy.miyauchi@gmail.com>
2022-03-27 02:08:40 +0900

    [add]スクレイピング表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  def scrapes(url)
+    require 'open-uri'
+
+    html = URI.open("#{url}").read
+    doc = Nokogiri::HTML.parse(html)
+    
+    href = doc.xpath("//h2[@class='eo-event-title entry-title']//a").map {|f| f.attribute("href").value}
+
+    href.map do |link|
+      # sleep 1
+      doc = Nokogiri::HTML(URI.open("#{link}"))
+      info = {  date: doc.xpath("//div[@class='date']").text,
+                main_title: doc.xpath("//div[@class='main_title']").text,
+                detail: doc.xpath("//div[@class='detail']").text.gsub!(/\n|\r|\r\n/, " / ")
+      }
+    end
+  end
+
 end
--- app/controllers/nokogiris_controller.rb
+++ app/controllers/nokogiris_controller.rb
@@ -1,4 +1,12 @@
 class NokogirisController < ApplicationController
   def index
+      year = Date.current.year
+      month = Date.current.month
+      @scrapes = []
+      3.times do |f|
+        url = "https://toos.co.jp/basementbar/event/on/#{year}/0#{month}/"
+        @scrapes << scrapes(url)
+        month += 1
+      end
   end
 end
